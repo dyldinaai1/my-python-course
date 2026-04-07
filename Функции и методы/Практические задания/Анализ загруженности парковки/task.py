b0c5e5e0f1a7379bf3ed2f4e7b79c6c660d738c1
dyldinaai1 <dyldina.ai@edu.spbstu.ru>
2026-04-08 00:47:54 +0300

    Delete Функции и методы directory

--- Функции и методы/Практические задания/Анализ загруженности парковки/task.py
+++
@@ -1,3 +0,0 @@
-def calculate_parking_load(total_parking_spaces, occupied_parking_spaces):
-    parking_load = (occupied_parking_spaces / total_parking_spaces) * 100
-    return round(parking_load)
--- Функции и методы/Практические задания/Построение лестницы/main.py
+++
@@ -1,7 +0,0 @@
-def print_stairs(count_stairs=4):
-    for i in range(1, count_stairs + 1):
-        print("*" * i)
-
-
-print_stairs()
-print_stairs(10)
