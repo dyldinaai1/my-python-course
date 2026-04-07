7dffc5cd6cdb1af007f4b8e7680821a92d6b0851
dyldinaai1 <dyldina.ai@edu.spbstu.ru>
2026-04-08 00:56:10 +0300

    Delete Условия и циклы directory

--- Условия и циклы/Условная инструкция if/Вложенные условные операторы/main.py
+++
@@ -1,9 +0,0 @@
-outside_condition = True
-inside_condition = True
-
-if outside_condition:
-    print("Условие истинно")
-    if inside_condition:
-        print("Ещё один условный оператор")
-else:
-    print("Условие ложно")
