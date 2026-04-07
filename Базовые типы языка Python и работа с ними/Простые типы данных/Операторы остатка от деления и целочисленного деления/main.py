9d3e8f885b7d5e93035edc1fc2b34d8b649706c2
dyldinaai1 <dyldina.ai@edu.spbstu.ru>
2026-04-08 00:43:47 +0300

    Delete Базовые типы языка Python и работа с ними directory

--- Базовые типы языка Python и работа с ними/Простые типы данных/Операторы остатка от деления и целочисленного деления/main.py
+++
@@ -1,8 +0,0 @@
-HOURS_IN_DAY = 24  # количество часов в дне постоянно
-total_hours = 5000  # дано по задаче
-
-days = total_hours // 24
-hours = total_hours % 24
-
-print("Количество дней =", days)
-print("Оставшееся количество часов =", hours)
