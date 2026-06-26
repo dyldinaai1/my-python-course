58eea549cb0ff3f04102c9a74113f0dc162c4ded
dyldinaai1 <dyldina.ai@edu.spbstu.ru>
2026-06-26 04:40:31 +0300

    Лабораторная: Добавлен ответ на задачу "Найти сумму произведений из списка словарей"

--- Работа с источниками данных/Лабораторная работа/Найти сумму произведений из списка словарей/main.py
+++ Работа с источниками данных/Лабораторная работа/Найти сумму произведений из списка словарей/main.py
@@ -1,6 +1,11 @@
-# TODO решите задачу
+import json
+import sys
+
 def task() -> float:
-    ...
+    with open('input.json', encoding='utf-8') as f:
+        data = json.load(f)
+    total = sum(item['score'] * item['weight'] for item in data)
+    return round(total, 3)
 
-
-print(task())
+if __name__ == '__main__':
+    print(task())
